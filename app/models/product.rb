0f511ad69607d4b692adf9617859ad2f0f35dbb0
Benco <bcos34@gmail.com>
2014-09-11 17:38:14 -0400

    Add validations and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+
+  validates :title, :price, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: { case_sensitive: false },
+                    length: { minimum: 10,
+                    message: 'must be a minimum of 10 characters long'
+                    }
+  VALID_IMAGE_REGEX = %r(\S\.(png|jpg|gif)\Z)i
+  validates :image_url, allow_blank: true, format: { 
+                  with: VALID_IMAGE_REGEX,
+                  message: 'must be a URL for JPG, PNG or GIF.'
+                  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,74 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(
+      title: "Lorem ipsum",
+      description: "It's good. Buy it.",
+      image_url: "image.png")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price =  1
+    assert product.valid?
+  end
+
+  test "image URL must be valid" do
+    def new_product(image_url)
+      product = Product.new(
+        title: "Lorem ipsum",
+        description: "It's good. Buy it.",
+        price: 1,
+        image_url: image_url)
+    end
+
+    ok = %w{ image.jpg i.jpg i.jPg png.png png.PnG picture.gif picture.GIF }
+
+    bad = %w{ .jpg xyz.xyz image_jpg .jpgimage .gif-mypic image.pngg }
+
+    ok.each do |url|
+      assert new_product(url).valid?, "#{url} should be valid"
+    end
+
+    bad.each do |url|
+      assert new_product(url).invalid?, "#{url} should be invalid"
+    end
+  end
+
+  test "product id is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product title must be a minimum of 10 characters long" do
+    product = Product.new(title: "SQL",
+                          description: "all about SQL",
+                          price: 1,
+                          image_url: "sql.jpg")
+    assert product.invalid?
+    assert_equal ["must be a minimum of 10 characters long"],
+      product.errors[:title]
+  end
 end
