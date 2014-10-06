8a429225af97f27b13dabb60963b3f36be6b61e2
Benco <bcos34@gmail.com>
2014-10-05 22:40:17 -0400

    Implement Order model and checkout functionality

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -70,6 +70,6 @@ class LineItemsControllerTest < ActionController::TestCase
       delete :destroy, id: @line_item
     end
     set_cart
-    assert_redirected_to @cart
+    assert_redirected_to store_url
   end
 end
