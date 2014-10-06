2512becbe98f93b246270a8bb57a8d44e708c25e
Benco <bcos34@gmail.com>
2014-10-06 18:33:20 -0400

    Incorporate Atom feed for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Depot::Application.routes.draw do
   resources :orders
-
   resources :line_items do
     member do
       post 'decrement'
@@ -8,7 +7,9 @@ Depot::Application.routes.draw do
   end
   resources :carts
   get "store/index"
-  resources :products
+  resources :products do
+    get :who_bought, on: :member #alternative syntax for route member
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
