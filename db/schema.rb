6f8134132adc2ab895822d2ae53b754b5b9ec570
Benco <bcos34@gmail.com>
2014-10-10 18:33:07 -0400

    Add ship_date column (in Date format) to orders table and shipped? selector to order form partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008005927) do
+ActiveRecord::Schema.define(version: 20141010211829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20141008005927) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "pay_type_id", default: 1
+    t.date     "ship_date"
   end
 
   create_table "pay_types", force: true do |t|
